a8587eb49d83c891cedd277cc04a0d2dd69c4692
Lorenzo Leva <lorenzodominik.leva@mail.polimi.it>
2022-05-12 10:50:57 +0200

    Added getNameText method.

--- app/src/ErrorRaiser.py
+++ app/src/ErrorRaiser.py
@@ -1,19 +1,33 @@
 
 class ErrorRaiser:
     @staticmethod
+    def getNameText(name=""):
+        if name is not "":
+            return " for " + name
+        else:
+            return ""
+
+    @staticmethod
     def raiseErrorOnlyInt(x, name=""):
-            if name is not "":
-                name = " for " + name
-            raise TypeError(f'Only integers are allowed. Received{name}:', type (x), x)
+            name = ErrorRaiser.getNameText(name)
+            
+            if type (x) is not int:
+                raise TypeError(f'Only integers are allowed. Received{name}: {type (x)} = {x}')
     
     @staticmethod
     def raiseNoNegativeInt(x, name=""):
-        if name is not "":
-            name = " for " + name
+        name = ErrorRaiser.getNameText(name)
         
-        if type (x) is not int:
-            ErrorRaiser.raiseErrorOnlyInt(x, name)
+        ErrorRaiser.raiseErrorOnlyInt(x, name)
 
         if x < 0:
             raise IndexError(f'No negative integer are allowed. Received{name}: {type (x)} = {x}')
         
+    @staticmethod
+    def raiseNoZeroNegativeInt(x, name=""):
+        name = ErrorRaiser.getNameText(name)
+        
+        ErrorRaiser.raiseErrorOnlyInt(x, name)
+
+        if x <= 0:
+            raise IndexError(f'No zero or negative integer are allowed. Received{name}: {type (x)} = {x}')
