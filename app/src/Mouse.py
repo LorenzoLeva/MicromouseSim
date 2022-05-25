6b4a101733d236f9be13c5ce28e188caf3ce9d54
Lorenzo Leva <lorenzodominik.leva@mail.polimi.it>
2022-05-25 12:04:44 +0200

    Added Mouse class. Added Mouse.setMaze method. Added Mouse.solve method. Added Mouse.solveMaze method. Added Mouse.setMaze test. Added Mouse.solveMaze test. Added Mouse.intit test.

--- app/src/Mouse.py
+++ app/src/Mouse.py
@@ -2,13 +2,48 @@ from Maze import Maze
 
 
 class Mouse:
-    def __init__(self, maze: Maze) -> None:
+    def __init__(self) -> None:
+        self.maze = None
+        self.visited = None
+        self.currentPosition = None
+        self.solution = None
+        
+    def solve(self, maze: Maze) -> None:
+        '''Makes the mouse solve the maze passed as params.
+        
+        Args:
+            maze (Maze): the maze to be solved by the mouse.
 
-        self.maze = Maze.raiseNotMaze(maze)
-        self.visited = [[False for x in range(self.maze.x)] for y in range(self.maze.y)]
+        Returns:
+            dict: dictionary containing the solution of the maze with some metadata
+
+            The dict that is returned has the following structure:
+                {
+                    solutionPath: array of coordinates in tuple form describing the path the mouse found.
+                }
+        '''
+
+        self.setMaze(maze)
+        self.solveMaze(maze)
+        return self.solution
 
         
 
+    def solveMaze(self):
+        '''This function solves the maze and generates self.solution dictionary containing all the information about the solution.
+        
+        Note:
+            The self.solution dictionary has to have the following structure:
+                {
+                    solutionPath: array of coordinates in tuple form describing the path the mouse found.
+                }
+        '''
+        raise NotImplementedError("The Maze class is an abstract class. Please implement the solveMaze method in a child class to be able to call Maze.solve.")
 
+    def setMaze(self, maze: Maze) -> None:
+        '''Sets up the mouse with all the Maze information it needs.'''
+        self.maze = Maze.raiseNotMaze(maze)
+        self.visited = [[False for x in range(self.maze.x)] for y in range(self.maze.y)]
 
-
+        self.currentPosition = self.maze.getStartPosition()
+        self.solution = {}
--- app/src/Mouse_test.py
+++ app/src/Mouse_test.py
@@ -5,22 +5,47 @@ from Mouse import Mouse
 from Maze import Maze
 
 class TestMouseInitialization(unittest.TestCase):
-     def test_invalid_input_types(self):
-        """Test if the Mouse class raises TypeError when initialized with the wrong parameter type."""
-
-        self.assertRaises(TypeError, Mouse, "x")
-
      def test_initialization(self):
         """Test if the Mouse class initializes correctly an object with all its properties correctly."""
-
-        columns = 3
-        rows = 4
-        seed = 123
-
-        maze = Maze(columns, rows, seed)
-        mouse = Mouse(maze)
-
-        self.assertEqual(mouse.maze, maze)
         
+        mouse = Mouse()
+
+        self.assertTrue(hasattr(mouse, "maze"))
+        self.assertTrue(hasattr(mouse, "visited"))
+        self.assertTrue(hasattr(mouse, "currentPosition"))
+        
+class TestSetMaze(unittest.TestCase):
+   def test_invalid_input_types(self):
+      """Test if the Mouse.setMaze method raises TypeError when called with the wrong parameter type."""
+
+      mouse = Mouse()
+
+      self.assertRaises(TypeError,  mouse.setMaze, "ABC")
+      self.assertRaises(TypeError,  mouse.setMaze, 2)
+      self.assertRaises(TypeError,  mouse.setMaze, True)
+
+
+   def test_valid_input(self):
+      """Test if the Mouse.setMaze method stores the values correctly."""
+
+      mouse = Mouse()
+      maze = Maze(16, 16)
+
+      mouse.setMaze(maze)
+
+      self.assertEqual(mouse.maze, maze)
+      self.assertEqual(len(mouse.visited), len(maze.maze))
+      self.assertEqual(len(mouse.visited[0]), len(maze.maze[0]))
+
+class TestSolveMaze(unittest.TestCase):
+   def test_valid_input(self):
+      """Test if the Mouse.solveMaze method stores the values correctly."""
+
+      mouse = Mouse()
+
+      self.assertRaises(NotImplementedError,  mouse.solveMaze)
+
+
+
 
         
