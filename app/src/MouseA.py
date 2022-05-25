92f64504db9f77af9e665ecd1e78f0303145ff84
Lorenzo Leva <lorenzodominik.leva@mail.polimi.it>
2022-05-25 12:06:18 +0200

    Deleted MouseA file. Deleted MouseA_test file. Deleted Maze dir and file within.

--- app/src/MouseA.py
+++
@@ -1,7 +0,0 @@
-from Mouse import Mouse
-from Maze import Maze
-
-class MouseA(Mouse):
-    def __init__(self, maze: Maze) -> None:
-        super().__init__(maze)
-        
--- app/src/MouseA_test.py
+++
@@ -1,2 +0,0 @@
-from MouseA import MouseA
-
