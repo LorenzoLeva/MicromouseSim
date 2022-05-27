0833b243432f66fdeadcaa6117649f1b3bdf3dbc
Lorenzo Leva <lorenzodominik.leva@mail.polimi.it>
2022-05-27 14:01:52 +0200

    Implemented Evaluator in Simulation class.

--- app/src/Simulation.py
+++ app/src/Simulation.py
@@ -1,12 +1,16 @@
 import abc
 
 from RandomWalker import RandomWalker
+from Mouse import Mouse
+from Evaluator import Evaluator
 
 
 class Simulation(metaclass= abc.ABCMeta):
-    def __init__(self, MouseType) -> None:
-        self.mouse = MouseType()
-    
+    def __init__(self, mouse: Mouse, EvaluatorType: Evaluator) -> None:
+        self.EvaluatorType = EvaluatorType
+
+        self.mouse = mouse
+
     @abc.abstractclassmethod
     def run(self):
         '''This function runs the simulation and returns the results of the Simulation.
--- app/src/SingleRunSimulation.py
+++ app/src/SingleRunSimulation.py
@@ -2,17 +2,23 @@ from Simulation import Simulation
 
 from RandomWalker import RandomWalker
 from DFS_R import DFS_R
+from Evaluator import Evaluator
+from Maze import Maze
+from Mouse import Mouse
 
 class SingleRunSimulation(Simulation):
-    def __init__(self, mouseType, MazeType) -> None:
-        super().__init__(mouseType)
-        self.maze = MazeType()
-        self.solution = None
+    def __init__(self, mouse: Mouse, MazeType: Maze, EvaluatorType: Evaluator) -> None:
+        super().__init__(mouse, EvaluatorType)
+        self.MazeType = MazeType
+        self.EvaluatorType = EvaluatorType
+
+        self.results = None
 
     def run(self):
-        self.maze.generate()
-        self.solution = self.mouse.solve(self.maze)
+        self.maze = self.MazeType()
+        self.evaluator = self.EvaluatorType(self.mouse, self.maze)
         
+        self.results = self.evaluator.evaluate()
         #TODO evaluate solution
 
 
